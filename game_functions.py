0d6b262c7f29c492e46d38b7aa8b1d1bcbd8813e
Idris Khan <idris.khan303@gmail.com>
2018-10-16 19:01:14 +0100

    added new function to print thr dialogue

--- game_functions.py
+++ game_functions.py
@@ -1,7 +1,18 @@
 # all game functions will be placed in this file
+import rooms
+import game_content as gc    # game_content is referred as 'gc'
+import player
+
 
 def print_room(room):
     """Will take a room as an argument, and display all of its contents"""
     print()
-    print('You are in the ' + room['name'])
+    print('You are in the ' + room['name'].upper())
+    print()
+
+
+def print_dialogue(dialogue):
+    """Will take a form of dialogue as argument, and display it properly"""
+    print()
+    print(dialogue.upper())
     print()
