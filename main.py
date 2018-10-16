18d726a003e5c02cc462bfb3c2dc708764653645
Idris Khan <idris.khan303@gmail.com>
2018-10-16 18:47:20 +0100

    The game will be ran from here,

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import game_functions as gf    # all functions are referred as 'gf' now
 from game_content import *
 
 # all game loops and events are tested and ran here
-
-current_room = rooms_id['lobby']
-print(opening_dialogue["description"])
-
 # main game loop
-while True:
-    # this is just a test to see if this function can run
-    gf.print_room(current_room)
-    break
+def game():
+    current_room = rooms_id['lobby']
+    print(opening_dialogue["description"])
+    while True:
+        # this is just a test to see if this function can run
+        gf.print_room(current_room)
+        break
+
+
+if __name__ == '__main__':
+    game()
 
 #test
 
