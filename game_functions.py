5f04a9fb82c123e19ed163e8ef79f1c674b2dbb3
Idris Khan <idris.khan303@gmail.com>
2018-10-16 16:00:58 +0100

    all the functions will run from

---
+++ game_functions.py
@@ -0,0 +1,7 @@
+# all game functions will be placed in this file
+
+def print_room(room):
+    """Will take a room as an argument, and display all of its contents"""
+    print()
+    print('You are in the ' + room['name'])
+    print()
