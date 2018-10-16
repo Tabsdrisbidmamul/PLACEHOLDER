1a3bc574eb3b1c962074f264fa931b361cf75d9d
Idris Khan <idris.khan303@gmail.com>
2018-10-16 16:54:10 +0100

    added import to game_contents

---
+++ game_content.py
@@ -0,0 +1,11 @@
+# contain all the storyline/ dialogue
+
+# Will contain the opening plot information
+opening_dialogue = {
+    "description":
+    """You wake up lying in a pool of blood on a hard wood 
+floor, staring up at a dimly lit chandelier covered in cobwebs on the 
+ceiling. Next to you is a heavy locked door. There are 4 keyholes. 
+How did you get here? You must escape.""",
+}
+
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from rooms import rooms_id
 from player import *
 import game_functions as gf    # all functions are referred as 'gf' now
+from game_content import *
 
 # all game loops and events are tested and ran here
 
 current_room = rooms_id['lobby']
+print(opening_dialogue["description"])
 
 # main game loop
 while True:
     # this is just a test to see if this function can run
     gf.print_room(current_room)
-
+    break
 
