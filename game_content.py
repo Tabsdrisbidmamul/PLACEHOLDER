0bd75aa9f90c2b395520acdc737e0fddbbc30478
Idris Khan <idris.khan303@gmail.com>
2018-10-16 16:54:45 +0100

    All game dialogue goes in this file

--- game_content.py
+++ game_content.py
@@ -3,6 +3,8 @@
 # Will contain the opening plot information
 opening_dialogue = {
     "description":
+
+    # all dialogue will follow this layout to outputted properly
     """You wake up lying in a pool of blood on a hard wood 
 floor, staring up at a dimly lit chandelier covered in cobwebs on the 
 ceiling. Next to you is a heavy locked door. There are 4 keyholes. 
