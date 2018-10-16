b08a9f10b83d12a183a88abe6718a49f38267e1e
Idris Khan <idris.khan303@gmail.com>
2018-10-16 16:01:39 +0100

    all room information in here

---
+++ rooms.py
@@ -0,0 +1,56 @@
+# all room details are in this file
+# room details have to be filled in
+room_lobby = {
+    'name': 'Main Lobby',
+
+    'description': 'the main landing of the house',
+
+    'items': [],
+
+}
+
+room_2 = {
+    'name': '',
+
+    'description': '',
+
+    'items': [],
+
+
+}
+
+room_3 = {
+    'name': '',
+
+    'description': '',
+
+    'items': [],
+
+}
+
+room_4 = {
+    'name': '',
+
+    'description': '',
+
+    'items': [],
+
+}
+
+room_5 = {
+    'name': '',
+
+    'description': '',
+
+    'items': [],
+
+}
+
+rooms_id = {
+    'lobby': room_lobby,
+    # a, b, c and d are placeholders for the room names
+    'a': room_2,
+    'b': room_3,
+    'c': room_4,
+    'd': room_5,
+}
