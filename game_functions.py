9a24517c36f661f968d04ad5f6197c81fb3f5afd
Idris Khan <idris.khan303@gmail.com>
2018-10-17 10:28:01 +0100

    update test

--- game_functions.py
+++ game_functions.py
@@ -41,43 +41,38 @@ def print_inventory(inventory):
             break
         store_string = store_string + value['name'] + ', '
     print('You have: ' + store_string)
-    
-def riddle_1():
-    
-    x= input("I'm tall when I'm young and I'm short when I'm old. What am I?")
-    if x="candle" or "a candle":
-        take(x)
-        
+
 def take(X):
     #take key and display:
-    print("                                                                                                    
-    print("              :sdmNNNds/`                       ")                                                    
-    print("            -dMNho//oyNMm/                         ")                                                 
-    print("           -NMd-      .yMM/                         ")                                                
-    print("           oMM:        `MMh                        ")                                                 
-    print("     `:oyhhmMMs        :MMy                        ")                                                 
-    print("   `omMNdhhmMMm:    .-sNMd--`.oo:                   ")                                                
-    print("   oMMh-   `:+.`sdyomMMMNdNMdmMMMoooooooooooo++++++oooooooooooooooooooooooooooooooooooooooo+-oso.   ")
-    print("   dMM-        /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMh   ")
-    print("   +MMh-`  `:+.`sdyomMMMNdNMdmMMMooooooooooooooooooooosssssssssssssssssssssssssdMMMMMMmyssys/yhs-   ")
-    print("    +mMNdhhmMMm:    .-sNMd--`.oo:                                         yyyyydMMMMMMmyyyyy`      ") 
-    print("     `:oyyymMMo        /MMs                                              `dNMMMMMMMMMMMMMMNd`     ")  
-    print("           sMM:        `MMh                                               /dMMMMMMMMMMMMMMd+`     ")  
-    print("           -NMd-      .hMM/                                              `MMMMNyyMMMMyyNMMMM.     ")  
-    print("            -dMNho//ohNMm/                                               `MMMMm  NMMM  dMMMM.     ")  
-    print("              :sdmNNNds/`                                                `mmmmh  dmmm  ymmmm`    ")   
-    print("")
-        print("")
-        print("")
-        print("")
-         print("")                                                                                            
+    
+    print(                                                                                                    
+              :sdmNNNds/`                                                                           
+            -dMNho//oyNMm/                                                                          
+           -NMd-      .yMM/                                                                         
+           oMM:        `MMh                                                                         
+     `:oyhhmMMs        :MMy                                                                         
+   `omMNdhhmMMm:    .-sNMd--`.oo:                                                                   
+   oMMh-   `:+.`sdyomMMMNdNMdmMMMoooooooooooo++++++oooooooooooooooooooooooooooooooooooooooo+-oso.   
+   dMM-        /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMh   
+   +MMh-`  `:+.`sdyomMMMNdNMdmMMMooooooooooooooooooooosssssssssssssssssssssssssdMMMMMMmyssys/yhs-   
+    +mMNdhhmMMm:    .-sNMd--`.oo:                                         yyyyydMMMMMMmyyyyy`       
+     `:oyyymMMo        /MMs                                              `dNMMMMMMMMMMMMMMNd`       
+           sMM:        `MMh                                               /dMMMMMMMMMMMMMMd+`       
+           -NMd-      .hMM/                                              `MMMMNyyMMMMyyNMMMM.       
+            -dMNho//ohNMm/                                               `MMMMm  NMMM  dMMMM.       
+              :sdmNNNds/`                                                `mmmmh  dmmm  ymmmm`       
+        
+        
+        
+        
+                                                                                                     
                                                                                                     
-     print("        `:`   :`  -/+/-   --    :`      `://:`   .://-  .//////.     `:.      `:   :` :////`--   --  s.   ")  
-     print("        od` sy .do-.-sd` sy    N:    `yy:..:/  yh-..+m-`.-yh..`    +om+      /m -h+  mo--- -m: :m.  N-    ") 
-     print("         +dsy  sh     m/ sy    N:    +d  .--- :N`    sh   sy         d+      /Nsy`   my++:  .m+m.   N-    ") 
-     print("          sd   sh     N/ sy    N:    +m  .-od :N`    yy   sy         d+      /m:d/   m+``    -M-    m.    ") 
-     print("          oh   .ds:-/hs  -m+-:sh      sh+::yd  sh/-:sh`   sy       -:ds:`    /m `sh. ms:::`  .M`   `o.    ") 
-     print("          `.     .::-`     -::`         -::.    `-:-`     ..       -----`    `.   .. .----`   -     :`    ") 
+`:`   :`  -/+/-   --    :`      `://:`   .://-  .//////.     `:.      `:   :` :////`--   --  s.     
+ od` sy .do-.-sd` sy    N:    `yy:..:/  yh-..+m-`.-yh..`    +om+      /m -h+  mo--- -m: :m.  N-     
+  +dsy  sh     m/ sy    N:    +d  .--- :N`    sh   sy         d+      /Nsy`   my++:  .m+m.   N-     
+   sd   sh     N/ sy    N:    +m  .-od :N`    yy   sy         d+      /m:d/   m+``    -M-    m.     
+   oh   .ds:-/hs  -m+-:sh      sh+::yd  sh/-:sh`   sy       -:ds:`    /m `sh. ms:::`  .M`   `o.     
+   `.     .::-`     -::`         -::.    `-:-`     ..       -----`    `.   .. .----`   -     :`     
         
         
         
--- rooms.py
+++ rooms.py
@@ -1,52 +1,86 @@
 # all room details are in this file
 # room details have to be filled in
-
 import items
+
+
 room_lobby = {
-    'name': 'Main Lobby',
+    'name': 'main lobby',
 
     'description': 'the main landing of the house',
 
     'items': [items.item1],
 
+    'exits': {'west': 'kitchen', 'east': 'bathroom', },
+
 }
 
 room_kitchen = {
-    'name': 'Kitchen',
+    'name': 'kitchen',
 
     'description': '',
 
     'items': [],
 
+    'exits': {'east': 'lobby', },
 
 }
 
 room_bathroom = {
-    'name': 'Bathroom',
+    'name': 'bathroom',
 
     'description': '',
 
     'items': [],
+
+    'exits': {'west', 'lobby'},
 
 }
 
 room_nursery = {
-    'name': 'Nursery',
+    'name': 'nursery',
 
     'description': '',
 
     'items': [],
+
+    'exits': {'east', 'landing floor', },
 
 }
 
 room_bedroom = {
-    'name': 'Bedroom',
+    'name': 'bedroom',
 
     'description': '',
 
     'items': [],
 
+    'exits': {'west': 'landing floor', },
+
+
 }
+
+room_stairs = {
+    'name': 'stairs',
+
+    'description': '',
+
+    'items': [],
+
+    'exits': {'south': 'lobby', 'north': 'landing floor', },
+
+}
+
+room_landing_floor_1 = {
+    'name': 'landing floor',
+
+    'description': '',
+
+    'items': [],
+
+    'exits': {'west': 'nursery', 'east': 'bedroom', },
+
+}
+
 
 rooms_id = {
     'lobby': room_lobby,
@@ -54,4 +88,7 @@ rooms_id = {
     'bathroom': room_bathroom,
     'nursery': room_nursery,
     'bedroom': room_bedroom,
+    'stairs': room_stairs,
+    'landing floor': room_landing_floor_1,
+
 }
