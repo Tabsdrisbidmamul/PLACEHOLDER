e7245d6cebc38a8ce0906c6fb5c90a4b28e062a3
Idris Khan <idris.khan303@gmail.com>
2018-10-17 09:47:21 +0100

    made current_room and current_dialogue are global now

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from rooms import rooms_id
-from player import *
+import player
 import game_functions as gf    # all functions are referred as 'gf' now
 import game_content as gc    # all dialogue are referred as 'gc' now
 
+# These variables are to be global
+current_room = rooms_id['lobby']
+current_dialogue = gc.opening_dialogue['description']
+
+
 # all game loops and events are tested and ran here
 # main game loop
-
-
 def game():
-    current_room = rooms_id['lobby']
-    current_dialogue = gc.opening_dialogue['description']
+
     while True:
         # this is just a test to see if this function can run
         gf.print_dialogue(current_dialogue)
         gf.print_room(current_room)
+        gf.print_inventory(player.inventory)
+        # don't take out
         break
 
 
