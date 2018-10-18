66483c563d60c0748344c97f2730728ef3ed6afc
Idris Khan <43910238+Tabsdrisbidmamul@users.noreply.github.com>
2018-10-18 13:28:46 +0100

    Added main() and still working on filtering words

--- game_functions.py
+++ game_functions.py
@@ -23,7 +23,12 @@ def print_dialogue(dialogue):
 
 def exit_leads_to(exits, direction):
     """This will take a dictionary of exits, and a direction from the user"""
-    return rooms.rooms_id[exits[direction]]['name']
+    return exits[direction]['name']
+
+
+def is_exit_valid(exits, direction):
+    if direction in exits:
+        return exits[direction]
 
 
 def move(exits, direction):
@@ -47,12 +52,12 @@ def print_inventory(inventory):
     print('You have: ' + store_string)
 
 
-def remove_whitespace(text):
+def no_whitespace(text):
     """will remove beginning and trailing whitespace from input"""
     return text.strip()
 
 
-def remove_punct(text):
+def no_punct(text):
     """will remove all forms of punctuation that is in the text"""
     punctuation = string.punctuation
     for letter in text:
@@ -61,27 +66,57 @@ def remove_punct(text):
     return text
 
 
+def refine_words(text):
+    disposable_words = [
+        'a', 'aa', 'aaa',  'an', 'and', 'the', 'be', 'to', 'of', 'in', 'that',
+        'have', 'i', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do',
+        'at', 'this', 'but', 'his', 'by', 'from', 'they', 'we', 'say', 'her',
+        'she', 'or', 'will', 'my', 'one', 'all' , 'would', 'there', 'their',
+        'what', 'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which', 'me',
+        'when', 'make', 'can', 'like', 'time', 'no', 'just', 'him', 'know',
+        'take', 'people', 'into', 'year', 'good', 'some', 'could', 'them',
+        'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its',
+        'over', 'think', 'also', 'back', 'after', 'two', 'how', 'our', 'work',
+        'first', 'well', 'way', 'even', 'new', 'want', 'because', 'any',
+        'these', 'give', 'day', 'most', 'us',
+    ]
+
+    with open('stop.txt', 'r') as f_obj:
+
+        words_wanted = []
+        for item in text:
+            if item not in disposable_words:
+                words_wanted.append(item)
+
+        return words_wanted
+
+
 def normal_input(user_input):
     """Will take the user input and firstly remove punctuation, then the
     whitespace and then lastly return it as a list, so it can therefore be
     processed in execute command"""
-    user_input = remove_punct(user_input)
-    user_input = remove_whitespace(user_input)
+    user_input = no_punct(user_input)
+    user_input = no_whitespace(user_input)
     normal_lst = user_input.split()
-    return normal_lst
+    filtered = refine_words(normal_lst)
+    return filtered
+
 
 def exe_go(user_input_command):
     pass
 
+
 def exe_take(user_input_command):
     pass
+
 
 def exe_solve_riddle(user_input_command):
     pass
 
 
 def exe_command(user_input_lst):
-    """function that will call either will go, take or solve riddle"""
+    """function will check the first item, in which it will then either call
+    go, take or solve riddle"""
     if len(user_input_lst) == 0:
         return None
 
@@ -109,9 +144,9 @@ def exe_command(user_input_lst):
 
 def riddle_1():
     x = input("I'm tall when I'm young and I'm short when I'm old. What am I?")
-    if x == ("candle") or ("a candle"):
-        take()
-        
+    if x == "candle" or "a candle":
+        pass
+"""    
 def take():
     #take key and display:
     
@@ -144,13 +179,30 @@ def take():
     print("           oh   .ds:-/hs  -m+-:sh      sh+::yd  sh/-:sh`   sy       -:ds:`    /m `sh. ms:::`  .M`   `o.     ")
     print("           `.     .::-`     -::`         -::.    `-:-`     ..       -----`    `.   .. .----`   -     :`   ")  
         
-        
+
+"""
+
 
 def print_menu():
-
+    """Will output the items in the room and also the player's inventory"""
     print("You can:")
     for values in current_room['items']:
         print("TAKE " + values.get('name'))
     for values in player.inventory:
         print("DROP " + values.get('name'))
 
+
+def main():
+    """Function will display the options the user has, it will then read
+    their input and go on to to the parser where their input will be
+    corrected for the exe commands to work correctly"""
+    # display the menu to the player
+    print_menu()
+
+    # take the user input
+    user_input = input('>')
+
+    # normal the input
+    normal_user_input = normal_input(user_input)
+
+    return normal_user_input
